e548730165071355135929407e7e4b68012d69a7
Evelyn Koo <r_evelynkoo@berkeley.edu>
2023-05-14 12:45:02 -0700

    i dont know what im doing and there's no way to debug it 3

--- lua/luasnip-latex-snippets/init.lua
+++ lua/luasnip-latex-snippets/init.lua
@@ -1,1 +1,5 @@
-require("luasnip.loaders.from_lua").load(debug.getinfo(1, "Sl").source .. "luasnippets")
+#!/bin/lua
+require("luasnip.loaders.from_lua").load(vim.api.nvim_get_runtime_file("luasnippets")) 
+
+-- info = debug.getinfo(1, "Sl")
+-- print(info.source:sub(2))
