faffe651fbdf4269ffae8ad0321b1b454d73bb9a
Evelyn Koo <r_evelynkoo@berkeley.edu>
2023-05-14 14:41:21 -0700

    i dont know what im doing and there's no way to debug it 7

--- lua/luasnip-latex-snippets/init.lua
+++ lua/luasnip-latex-snippets/init.lua
@@ -1,4 +1,5 @@
 #!/bin/lua
+require("luasnip").log.set_loglevel("info")
 require("luasnip.loaders.from_lua").load({paths = debug.getinfo(1).source:sub(2) .. "luasnippets"})
 
 -- info = debug.getinfo(1, "Sl")
