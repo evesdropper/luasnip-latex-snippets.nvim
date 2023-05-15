c963fb03cbbaf3d591ff973a3dc0d0ed170bb4cd
Evelyn Koo <r_evelynkoo@berkeley.edu>
2023-05-15 09:59:01 -0700

    rtfm

--- lua/luasnip-latex-snippets/init.lua
+++ lua/luasnip-latex-snippets/init.lua
@@ -1,9 +1,10 @@
 #!/bin/lua
-local ls = require("luasnip")
-ls.log.ping()
-ls.log.set_loglevel("debug")
-vim.pretty_print(debug.getinfo(1).source:sub(2):gsub("init.lua", ""))
-require("luasnip.loaders.from_lua").load({paths = debug.getinfo(1).source:sub(2):gsub("init.lua", "") .. "luasnippets"})
+-- local ls = require("luasnip")
+-- ls.log.ping()
+-- ls.log.set_loglevel("debug")
+-- vim.pretty_print(debug.getinfo(1).source:sub(2):gsub("init.lua", ""))
+snippet_path = debug.getinfo(1).source:sub(2):gsub("init.lua", "luasnippets")
+require("luasnip.loaders.from_lua").load({paths = snippet_path })
 
--- info = debug.getinfo(1, "Sl")
--- print(info.source:sub(2))
+-- info = debug.getinfo(1)
+-- print(info.source:sub(2):gsub("init.lua", ""))
