56707233f7c5e4f4fa1950dc6d3bd51b81c0e72a
Evelyn Koo <r_evelynkoo@berkeley.edu>
2023-05-15 00:06:57 -0700

    i dont know what i'm doing 10

--- lua/luasnip-latex-snippets/init.lua
+++ lua/luasnip-latex-snippets/init.lua
@@ -2,7 +2,7 @@
 local ls = require("luasnip")
 ls.log.ping()
 ls.log.set_loglevel("debug")
-require("luasnip.loaders.from_lua").load({paths = debug.getinfo(1).source:sub(2) .. "luasnippets"})
+require("luasnip.loaders.from_lua").load({paths = debug.getinfo(1).source:sub(2):gsub("init.lua", "") .. "luasnippets"})
 
 -- info = debug.getinfo(1, "Sl")
 -- print(info.source:sub(2))
