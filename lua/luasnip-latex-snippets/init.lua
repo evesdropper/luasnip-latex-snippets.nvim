8be8b508e2d205ab7d01af3c6ad4ae51f39ee52c
Evelyn Koo <r_evelynkoo@berkeley.edu>
2023-05-15 10:58:58 -0700

    rtfm ?

--- lua/luasnip-latex-snippets/init.lua
+++ lua/luasnip-latex-snippets/init.lua
@@ -4,7 +4,7 @@
 -- ls.log.set_loglevel("debug")
 -- vim.pretty_print(debug.getinfo(1).source:sub(2):gsub("init.lua", ""))
 snippet_path = debug.getinfo(1).source:sub(2):gsub("init.lua", "luasnippets")
-require("luasnip.loaders.from_lua").load({paths = snippet_path })
+require("luasnip.loaders.from_lua").lazy_load({ paths = snippet_path })
 
 -- info = debug.getinfo(1)
 -- print(info.source:sub(2):gsub("init.lua", ""))
