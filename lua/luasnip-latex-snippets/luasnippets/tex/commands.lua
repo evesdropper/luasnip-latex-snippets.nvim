a5bec386c6344c68e7821f018cb6da2a2fd0d207
Evelyn Koo <r_evelynkoo@berkeley.edu>
2023-05-17 10:53:54 -0700

    ?

--- lua/luasnip-latex-snippets/luasnippets/tex/commands.lua
+++ lua/luasnip-latex-snippets/luasnippets/tex/commands.lua
@@ -36,25 +36,4 @@ local reference_snippet_table = {
     r = ""
 }
 
-return {
-    --[
-    -- Labels/References
-    --]
-    -- personally perfer \label{type:number/value}
-    autosnippet({ trig='alab', name='label', dscr='add a label'},
-    fmta([[
-    \label{<>:<>}
-    ]],
-    { i(1), i(0) }),
-    { condition = tex.in_text, show_condition = tex.in_text }),
 
-    autosnippet({ trig='([acCer])ref', name='(acC|eq)?ref', dscr='add a reference (with autoref, cref, eqref)', regTrig = true, hidden = true},
-    fmta([[
-    \<>ref{<>:<>}
-    ]],
-    { f(function (_, snip)
-        return reference_snippet_table[snip.captures[1]]
-    end),
-    i(1), i(0) })
-    { condition = tex.in_text, show_condition = tex.in_text }),
-}
