d5065ae6520d4f1532c9e96d595837c74ba4c0ec
Evelyn Koo <r_evelynkoo@berkeley.edu>
2023-05-17 10:35:39 -0700

    testing

--- lua/luasnip-latex-snippets/luasnippets/tex/utils/init.lua
+++
@@ -1,6 +0,0 @@
-local M = {}
-
-M.conditions = require("luasnip-latex-snippets.luasnippets.tex.utils.conditions")
-M.scaffolding = require("luasnip-latex-snippets.luasnippets.tex.utils.scaffolding")
-
-return M
