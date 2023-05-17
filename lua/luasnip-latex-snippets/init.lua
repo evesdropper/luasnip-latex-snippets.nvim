74ebce84b94b4f9f8a374cf954b1d3afe7679925
Evelyn Koo <r_evelynkoo@berkeley.edu>
2023-05-17 09:25:19 -0700

    gpt

--- lua/luasnip-latex-snippets/init.lua
+++ lua/luasnip-latex-snippets/init.lua
@@ -4,6 +4,7 @@
 -- ls.log.set_loglevel("debug")
 -- vim.pretty_print(debug.getinfo(1).source:sub(2):gsub("init.lua", ""))
 snippet_path = debug.getinfo(1).source:sub(2):gsub("init.lua", "luasnippets")
+print(snippet_path)
 require("luasnip.loaders.from_lua").lazy_load({ paths = snippet_path })
 
 -- info = debug.getinfo(1)
